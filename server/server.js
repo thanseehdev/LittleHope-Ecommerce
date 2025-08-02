dac5c2e39f8e09bb870cfd40f3f45f914f8beb0f
thanseehdev <thanseehdev@gmail.com>
2025-08-03 00:10:22 +0530

    connecct db is server done for vercel deployment

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const cors = require('cors');
-const app=express()
+const app = express()
 require('dotenv').config();
-const {connectDB,getDB}=require('./config/db/db.connection')
-const userRoute=require('./routes/userRoute/userRoutes')
-const adminRoute=require('./routes/adminRoute/adminRoute')
+const { connectDB } = require('./config/db/db.connection')
+const userRoute = require('./routes/userRoute/userRoutes')
+const adminRoute = require('./routes/adminRoute/adminRoute')
 const cookieParser = require('cookie-parser')
 const session = require('express-session');
 
@@ -20,31 +20,23 @@ app.use(session({
   secret: process.env.SESSIONPASS,
   resave: false,
   saveUninitialized: false,
-  cookie: { 
+  cookie: {
     secure: process.env.NODE_ENV === 'production',
-    maxAge: 10 * 60 * 1000 
+    maxAge: 10 * 60 * 1000
   }
 }));
 
-async function initializeDatabase() {
-    try {
-        await connectDB()
-        console.log('database connected successfully')
-    } catch (error) {
-        console.log(`error while connecting to database : ${error}`)
-    }
-}
+connectDB()
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("welcome to littlehope API")
 })
 
-app.use('/api/user',userRoute)
-app.use('/api/admin',adminRoute)
+app.use('/api/user', userRoute)
+app.use('/api/admin', adminRoute)
 
 
-app.listen(process.env.PORT,async()=>{
-    await initializeDatabase()
-    console.log(`server is running on port ${process.env.PORT}`)
+app.listen(process.env.PORT, async () => {
+  console.log(`server is running on port ${process.env.PORT}`)
 })
 
