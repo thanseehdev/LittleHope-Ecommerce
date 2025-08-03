a23d5291a46a1b2c926387c5708c21403e1caf55
thanseehdev <thanseehdev@gmail.com>
2025-08-03 14:41:24 +0530

    some changes in server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const cors = require('cors');
-const app = express()
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
+const app = express()
 const { connectDB } = require('./config/db/db.connection')
 const userRoute = require('./routes/userRoute/userRoutes')
 const adminRoute = require('./routes/adminRoute/adminRoute')
-const cookieParser = require('cookie-parser')
+
+
+
+
+app.use(cors({
+  origin: 'https://littlehope.online',
+  credentials: true,
+}));
 
 
 app.use(express.json());
-app.use(cookieParser())
-
-app.use(cors({
-  origin: ['https://littlehope.online'],
-  credentials: true
-}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 connectDB()
